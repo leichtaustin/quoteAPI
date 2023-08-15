fca041726246480b49322080acdf01fd7ac3733f
Austin Leicht <leichtaustin@gmail.com>
2023-08-15 13:19:23 -0600

    added get random quote element

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    res.send(quotes);
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = {
+        quote: getRandomElement(quotes).quote,
+    };
+
+    if(randomQuote) {
+        res.send(randomQuote);
+    } else {
+        res.status(404).send('Unable to find quote');
+    }
+})
